88dbe7cd23c16e19118898bb5dd0ec96b3bdc3d6
Jp <justin.obfuscate@gmail.com>
2022-05-25 10:29:08 -0500

    retry webhook with persistence
    retry webhook with persistence

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","National Parks", new Coordinates("40.039315", "10.505167"), 4);
+        return new Backend("nationalparks","National Parks", new Coordinates("40.039310", "10.505160"), 4);
     }
 }
